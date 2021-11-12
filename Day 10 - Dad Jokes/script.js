9517d071764ea61f7538bf3472414707d3dab21b
Ar1f007 <muhammadarif9416@yahoo.com>
2021-11-12 15:11:40 +0600

    Dad Jokes functionality

---
+++ Day 10 - Dad Jokes/script.js
@@ -0,0 +1,30 @@
+const jokeEl = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+// const generateJoke = () => {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com/', config)
+//     .then((res) => res.json())
+//     .then((data) => (jokeEl.innerHTML = data.joke));
+// };
+
+const generateJoke = async () => {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const res = await fetch('https://icanhazdadjoke.com/', config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+};
+
+generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
