95e1d8e508b8ad97ea0096b57516cf79f5aea43a
Ar1f007 <muhammadarif9416@yahoo.com>
2021-11-25 00:04:50 +0600

    drink water functionality

--- Day 16 - Drink Water/script.js
+++ Day 16 - Drink Water/script.js
@@ -1,25 +1,52 @@
 const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
 
-const liters = document.querySelector('#liters');
-const percentage = document.querySelector('#percentage');
-const remained = document.querySelector('#remained');
+updateBigCup();
 
-smallCups.forEach((cup, i) => {
-  cup.addEventListener('click', () => highlightCups(i));
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx));
 });
 
-function highlightCups(i) {
-  if (
-    smallCups[i].classList.contains('full') &&
-    !smallCups[i].nextElementSibling.classList.contains('full')
+function highlightCups(idx) {
+  if (idx === 7 && smallCups[idx].classList.contains('full')) idx--;
+  else if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
   ) {
-    i--;
+    idx--;
   }
-  smallCups.forEach((cup, idx) => {
-    if (idx <= i) {
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
       cup.classList.add('full');
     } else {
       cup.classList.remove('full');
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
