c190981e501b9ec1dea8ddeb6aa1ff2b31ea8483
Ar1f007 <muhammadarif9416@yahoo.com>
2021-11-27 17:57:01 +0600

    movie app api setup

---
+++ Day 17 - Movie App/script.js
@@ -0,0 +1,4 @@
+const API_KEY = '9b41cfdb4d5a5a4698dcb119c9d34d7d';
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
