701e7aa9d97eff94c7d3cbfdbf68c406b7489f81
Ar1f007 <muhammadarif9416@yahoo.com>
2021-12-30 15:29:52 +0600

    Dynamically render profile

--- Day 28 - Github Profile/script.js
+++ Day 28 - Github Profile/script.js
@@ -2,14 +2,59 @@ const API_URL = 'https://api.github.com/users/';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 async function getUser(username) {
   try {
     const { data } = await axios.get(API_URL + username);
-    console.log(data);
+    createUserCard(data);
   } catch (error) {
-    console.log(error);
+    if (error.response.status == 404)
+      createErrorCard('No profile with this username');
   }
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+  <div class="card">
+    <h1>${msg}</h1>
+  </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+   <div class="card">
+        <div>
+          <img
+            src="${user.avatar_url}"
+            alt="${user.name}"
+            class="avatar"
+          />
+        </div>
+
+        <div class="user-info">
+          <h2>${user.name}</h2>
+          <p>
+          ${user.bio ? user.bio : ''}
+          </p>
+          <ul>
+            <li>${user.followers}<strong> Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+          </ul>
+          <div class="repos">
+            <a href="#" class="repo">Repo 1</a>
+            <a href="#" class="repo">Repo 2</a>
+            <a href="#" class="repo">Repo 3</a>
+          </div>
+        </div>
+      </div>
+  `;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e) => {
