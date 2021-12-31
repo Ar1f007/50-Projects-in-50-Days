89883be7613fb562b1256244096435688902502c
Ar1f007 <muhammadarif9416@yahoo.com>
2021-12-31 14:58:45 +0600

    fetching repos- github profile

--- Day 28 - Github Profile/script.js
+++ Day 28 - Github Profile/script.js
@@ -8,9 +8,22 @@ async function getUser(username) {
   try {
     const { data } = await axios.get(API_URL + username);
     createUserCard(data);
+    getRepos(username);
   } catch (error) {
     if (error.response.status == 404)
       createErrorCard('No profile with this username');
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios.get(
+      API_URL + username + '/repos?sort=created'
+    );
+
+    addReposToCard(data);
+  } catch (error) {
+    createErrorCard('no repos');
   }
 }
 
@@ -45,16 +58,28 @@ function createUserCard(user) {
             <li>${user.following} <strong>Following</strong></li>
             <li>${user.public_repos} <strong>Repos</strong></li>
           </ul>
-          <div class="repos">
-            <a href="#" class="repo">Repo 1</a>
-            <a href="#" class="repo">Repo 2</a>
-            <a href="#" class="repo">Repo 3</a>
+          <div id="repos">
+           
           </div>
         </div>
       </div>
   `;
 
   main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos');
+
+  repos.slice(0, 5).forEach((repo) => {
+    const repoEl = document.createElement('a');
+    repoEl.classList.add('repo');
+    repoEl.href = repo.html_url;
+    repoEl.target = '_blank';
+    repoEl.innerText = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
 }
 
 form.addEventListener('submit', (e) => {
