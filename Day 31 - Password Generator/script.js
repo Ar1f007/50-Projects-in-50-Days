a7833c32839b56a98963cd0060dfa6bc2638c6e0
Ar1f007 <muhammadarif9416@yahoo.com>
2022-01-11 00:08:44 +0600

    Generate password

--- Day 31 - Password Generator/script.js
+++ Day 31 - Password Generator/script.js
@@ -1,3 +1,56 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('pass-length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+  lower: getRandomLowercaseLtr,
+  upper: getRandomUppercaseLtr,
+  number: getRandomNum,
+  symbol: getRandomSymbol,
+};
+
+generateEl.addEventListener('click', () => {
+  const length = lengthEl.value * 1;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) return '';
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
 function getRandomLowercaseLtr() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
