55361fdb9522c78d0c5a5cd2c2be417eb248ce2f
Ar1f007 <muhammadarif9416@yahoo.com>
2022-02-18 19:22:56 +0600

    3D boxes functionality

---
+++ Day 40 - 3D Boxes Background/script.js
@@ -0,0 +1,16 @@
+const boxesContainer = document.querySelector('#boxes');
+const btn = document.querySelector('#btn');
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxesContainer.appendChild(box);
+    }
+  }
+}
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'));
+createBoxes();
